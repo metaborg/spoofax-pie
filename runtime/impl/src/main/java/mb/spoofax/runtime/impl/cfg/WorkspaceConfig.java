6d445c17a70733ff8b619ed926349cda93d9164f
Gabriel Konat <gabrielkonat@gmail.com>
2017-10-31 16:08:03 +0100

    Rewrite WorkspaceConfig to not serialize HashSets and HashMaps, fixing serialization issues in PIE.

--- runtime/impl/src/main/java/mb/spoofax/runtime/impl/cfg/WorkspaceConfig.java
+++ runtime/impl/src/main/java/mb/spoofax/runtime/impl/cfg/WorkspaceConfig.java
@@ -1,80 +1,91 @@
 package mb.spoofax.runtime.impl.cfg;
 
-import java.io.Serializable;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import javax.annotation.Nullable;
-
+import mb.spoofax.runtime.model.SpoofaxEx;
 import org.immutables.serial.Serial;
 import org.immutables.value.Value;
 
-import mb.spoofax.runtime.model.SpoofaxEx;
+import javax.annotation.Nullable;
+import java.io.Serializable;
+import java.util.*;
 
 @Value.Immutable
 @Serial.Version(value = 1L)
 public interface WorkspaceConfig extends Serializable {
-    Map<String, LangSpecConfig> langSpecConfigPerExt();
+    List<LangSpecConfig> langSpecConfigs();
 
-    Map<String, SpxCoreConfig> spxCoreConfigPerExt();
-
-    Set<String> extensions();
+    List<SpxCoreConfig> spxCoreConfigs();
 
 
-    public static WorkspaceConfig fromConfigs(Iterable<LangSpecConfig> langSpecConfigs,
-        Iterable<SpxCoreConfig> spxCoreConfigs) throws SpoofaxEx {
+    @Value.Lazy default Map<String, LangSpecConfig> langSpecConfigPerExt() {
+        final Map<String, LangSpecConfig> map = new HashMap<>();
+        for(LangSpecConfig config : langSpecConfigs()) {
+            for(String extension : config.extensions()) {
+                map.put(extension, config);
+            }
+        }
+        return map;
+    }
+
+    @Value.Lazy default Map<String, SpxCoreConfig> spxCoreConfigPerExt() {
+        final Map<String, SpxCoreConfig> map = new HashMap<>();
+        for(SpxCoreConfig config : spxCoreConfigs()) {
+            for(String extension : config.extensions()) {
+                map.put(extension, config);
+            }
+        }
+        return map;
+    }
+
+    @Value.Lazy default Set<String> extensions() {
+        final Set<String> extensions = new HashSet<>();
+        extensions.addAll(langSpecConfigPerExt().keySet());
+        extensions.addAll(spxCoreConfigPerExt().keySet());
+        return extensions;
+    }
+
+
+    static WorkspaceConfig fromConfigs(Iterable<LangSpecConfig> langSpecConfigs, Iterable<SpxCoreConfig> spxCoreConfigs)
+        throws SpoofaxEx {
         final ImmutableWorkspaceConfig.Builder builder = ImmutableWorkspaceConfig.builder();
         final Set<String> extensions = new HashSet<>();
         for(LangSpecConfig config : langSpecConfigs) {
+            builder.addLangSpecConfigs(config);
             for(String extension : config.extensions()) {
                 if(extensions.contains(extension)) {
                     throw new SpoofaxEx("Cannot add config '" + config + "' with extension '" + extension
                         + "', that extension is already in use");
                 }
                 extensions.add(extension);
-                builder.putLangSpecConfigPerExt(extension, config);
             }
         }
         for(SpxCoreConfig config : spxCoreConfigs) {
+            builder.addSpxCoreConfigs(config);
             for(String extension : config.extensions()) {
                 if(extensions.contains(extension)) {
                     throw new SpoofaxEx("Cannot add config '" + config + "' with extension '" + extension
                         + "', that extension is already in use");
                 }
                 extensions.add(extension);
-                builder.putSpxCoreConfigPerExt(extension, config);
             }
         }
-        builder.addAllExtensions(extensions);
         return builder.build();
     }
 
 
-    public default Collection<LangSpecConfig> langSpecConfigs() {
-        return langSpecConfigPerExt().values();
-    }
-
-    public default Collection<SpxCoreConfig> spxCoreConfigs() {
-        return spxCoreConfigPerExt().values();
-    }
-
-
-    public default Set<String> langSpecExtensions() {
+    default Set<String> langSpecExtensions() {
         return langSpecConfigPerExt().keySet();
     }
 
-    public default Set<String> spxCoreExtensions() {
+    default Set<String> spxCoreExtensions() {
         return spxCoreConfigPerExt().keySet();
     }
 
 
-    public default @Nullable LangSpecConfig langSpecConfigForExt(String extension) {
+    default @Nullable LangSpecConfig langSpecConfigForExt(String extension) {
         return langSpecConfigPerExt().get(extension);
     }
 
-    public default @Nullable SpxCoreConfig spxCoreConfigForExt(String extension) {
+    default @Nullable SpxCoreConfig spxCoreConfigForExt(String extension) {
         return spxCoreConfigPerExt().get(extension);
     }
 }
