3fb9a67b5381dacea4296f6351f4e459e15843c8
Gabriel Konat <gabrielkonat@gmail.com>
2018-10-18 15:33:55 +0200

    Fix typo.

--- set_spoofax_pie_version.sh
+++ set_spoofax_pie_version.sh
@@ -7,6 +7,6 @@ NEW_VERSION=$2
 
 mvn -f pom/pom.xml versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false
 
-perl -pi -e "s/$OLD_VERSION/$NEW_VERSION/g" lang/cfg/langpec/metaborg.yaml
+perl -pi -e "s/$OLD_VERSION/$NEW_VERSION/g" lang/cfg/langspec/metaborg.yaml
 perl -pi -e "s/$OLD_VERSION/$NEW_VERSION/g" lang/cfg/example/metaborg.yaml
 perl -pi -e "s/$OLD_VERSION/$NEW_VERSION/g" lang/cfg/test/metaborg.yaml
