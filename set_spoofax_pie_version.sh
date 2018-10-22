cce507099b7cfb85f946707d82d575f989803848
Gabriel Konat <gabrielkonat@gmail.com>
2018-10-22 15:34:33 +0200

    Update version change script to properly set Eclipse versions. Build Eclipse plugin in Travis build.

--- set_spoofax_pie_version.sh
+++ set_spoofax_pie_version.sh
@@ -2,11 +2,16 @@
 
 set -eu
 
-OLD_VERSION=$1
-NEW_VERSION=$2
+OLD_MAVEN_VERSION=$1
+NEW_MAVEN_VERSION=$2
+OLD_ECLIPSE_VERSION=$3
+NEW_ECLIPSE_VERSION=$4
 
-mvn -f pom/pom.xml versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false
+mvn -f pom/pom.xml versions:set -DnewVersion=$NEW_MAVEN_VERSION -DgenerateBackupPoms=false
 
-perl -pi -e "s/$OLD_VERSION/$NEW_VERSION/g" lang/cfg/langspec/metaborg.yaml
-perl -pi -e "s/$OLD_VERSION/$NEW_VERSION/g" lang/cfg/example/metaborg.yaml
-perl -pi -e "s/$OLD_VERSION/$NEW_VERSION/g" lang/cfg/test/metaborg.yaml
+perl -pi -e "s/$OLD_MAVEN_VERSION/$NEW_MAVEN_VERSION/g" pie.eclipse/pom.xml
+perl -pi -e "s/$OLD_ECLIPSE_VERSION/$NEW_ECLIPSE_VERSION/g" pie.eclipse/META-INF/MANIFEST.MF
+
+perl -pi -e "s/$OLD_MAVEN_VERSION/$NEW_MAVEN_VERSION/g" lang/cfg/langspec/metaborg.yaml
+perl -pi -e "s/$OLD_MAVEN_VERSION/$NEW_MAVEN_VERSION/g" lang/cfg/example/metaborg.yaml
+perl -pi -e "s/$OLD_MAVEN_VERSION/$NEW_MAVEN_VERSION/g" lang/cfg/test/metaborg.yaml
