c4bdca11466f2142a06a88a0050689488cd661ee
Gabriel Konat <gabrielkonat@gmail.com>
2019-04-02 11:48:50 +0200

    Cleanup.

--- spoofax.core/src/main/java/mb/spoofax/core/platform/FileSystemResourceServiceModule.java
+++ spoofax.core/src/main/java/mb/spoofax/core/platform/FileSystemResourceServiceModule.java
@@ -3,10 +3,15 @@ package mb.spoofax.core.platform;
 import dagger.Module;
 import dagger.Provides;
 import mb.fs.api.FileSystem;
+import mb.fs.java.JavaFileSystem;
 
 @Module
 public class FileSystemResourceServiceModule {
     private final FileSystem fileSystem;
+
+    public FileSystemResourceServiceModule() {
+        this(JavaFileSystem.instance);
+    }
 
     public FileSystemResourceServiceModule(FileSystem fileSystem) {
         this.fileSystem = fileSystem;
--- tiger.cmd/src/main/java/mb/tiger/cmd/Main.java
+++ tiger.cmd/src/main/java/mb/tiger/cmd/Main.java
@@ -1,13 +1,11 @@
 package mb.tiger.cmd;
 
-import mb.fs.java.JavaFileSystem;
 import mb.jsglr1.common.JSGLR1ParseTableException;
 import mb.pie.dagger.DaggerPieComponent;
 import mb.pie.dagger.DaggerResourceSystemsComponent;
 import mb.pie.dagger.PieComponent;
 import mb.spoofax.cmd.SpoofaxCmd;
 import mb.spoofax.core.platform.DaggerPlatformComponent;
-import mb.spoofax.core.platform.FileSystemResourceServiceModule;
 import mb.spoofax.core.platform.PlatformComponent;
 import mb.tiger.spoofax.DaggerTigerComponent;
 import mb.tiger.spoofax.TigerComponent;
@@ -17,10 +15,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, JSGLR1ParseTableException {
-        final PlatformComponent platformComponent = DaggerPlatformComponent
-            .builder()
-            .fileSystemResourceServiceModule(new FileSystemResourceServiceModule(JavaFileSystem.instance))
-            .build();
+        final PlatformComponent platformComponent = DaggerPlatformComponent.create();
         final TigerComponent tigerComponent = DaggerTigerComponent
             .builder()
             .platformComponent(platformComponent)
