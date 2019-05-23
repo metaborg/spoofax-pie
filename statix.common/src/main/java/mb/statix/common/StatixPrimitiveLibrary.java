abf36e260521dfeaf4736c817ca78fb948e67f0f
Gabriel Konat <gabrielkonat@gmail.com>
2019-05-23 13:06:22 +0200

    Add Statix primitives.

---
+++ statix.common/src/main/java/mb/statix/common/StatixPrimitiveLibrary.java
@@ -0,0 +1,24 @@
+package mb.statix.common;
+
+import mb.statix.spoofax.STX_compare_patterns;
+import mb.statix.spoofax.STX_delays_as_errors;
+import mb.statix.spoofax.STX_extract_messages;
+import mb.statix.spoofax.STX_solve_constraint;
+import mb.statix.spoofax.STX_solve_multi_file;
+import mb.statix.spoofax.STX_solve_multi_project;
+import org.spoofax.interpreter.library.AbstractStrategoOperatorRegistry;
+
+public class StatixPrimitiveLibrary extends AbstractStrategoOperatorRegistry {
+    public StatixPrimitiveLibrary() {
+        add(new STX_compare_patterns());
+        add(new STX_delays_as_errors());
+        add(new STX_extract_messages());
+        add(new STX_solve_constraint());
+        add(new STX_solve_multi_file());
+        add(new STX_solve_multi_project());
+    }
+
+    @Override public String getOperatorRegistryName() {
+        return "Statix";
+    }
+}
