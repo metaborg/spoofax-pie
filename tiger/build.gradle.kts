8a8e510b5e6b079d2f6ce5389189f48bc2ff2ca1
Gabriel Konat <gabrielkonat@gmail.com>
2019-06-04 15:35:37 +0200

    Improve how resources are copied into tiger JAR.

--- tiger/build.gradle.kts
+++ tiger/build.gradle.kts
@@ -21,50 +21,46 @@ dependencies {
   testCompileOnly("org.checkerframework:checker-qual-android")
 }
 
-// Copy files from org.metaborg.lang.tiger into src/main/resources.
-repositories {
-  // Required repositories for Tiger dependency.
-  maven("https://pluto-build.github.io/mvnrepository/")
-  maven("https://sugar-lang.github.io/mvnrepository/")
-  maven("http://nexus.usethesource.io/content/repositories/public/")
+// Copy select files from the language into the resulting JAR file
+run {
+  // Create dependency to spoofax-language artifact.
+  val dependency = run {
+    val dep = dependencies.project(":org.metaborg.lang.tiger", Dependency.DEFAULT_CONFIGURATION)
+    dep.isTransitive = false // Don't care about transitive dependencies, just want the Tiger spoofax-language artifact.
+    dep.artifact {
+      name = dep.name
+      type = "spoofax-language"
+      extension = "spoofax-language"
+    }
+    dep
+  }
+  // Create 'resources' configuration that contains the dependency.
+  val configuration = configurations.create("resources") {
+    dependencies.add(dependency)
+  }
+  // First unpack Tiger language resources, because we cannot copy from a subdirectory in a ziptree.
+  val unpackTask = tasks.register<Sync>("unpackResources") {
+    dependsOn(configuration)
+    from({ configuration.map { project.zipTree(it) } })  /* Closure inside `from` to defer evaluation until task execution time */
+    into("$buildDir/unpacked")
+    include("target/metaborg/editor.esv.af", "target/metaborg/sdf.tbl", "target/metaborg/stratego.jar", "target/metaborg/stratego-javastrat.jar")
+  }
+  // Copy resources into `mainSourceSet.java.outputDir` and `testSourceSet.java.outputDir`, so the resources finally end up in the 'mb.tiger' package in the resulting JAR.
+  val copySpec = copySpec {
+    from("$buildDir/unpacked/target/metaborg")
+    include("editor.esv.af", "sdf.tbl", "stratego.jar", "stratego-javastrat.jar")
+  }
+  val destPackage = "mb/tiger"
+  val syncMainTask = tasks.register<Sync>("syncMainResources") {
+    dependsOn(unpackTask)
+    with(copySpec)
+    into(sourceSets.main.get().java.outputDir.resolve(destPackage))
+  }
+  tasks.getByName(JavaPlugin.CLASSES_TASK_NAME).dependsOn(syncMainTask)
+  val syncTestTask = tasks.register<Sync>("syncTestResources") {
+    dependsOn(unpackTask)
+    with(copySpec)
+    into(sourceSets.test.get().java.outputDir.resolve(destPackage))
+  }
+  tasks.getByName(JavaPlugin.TEST_CLASSES_TASK_NAME).dependsOn(syncTestTask)
 }
-val tigerDependency = dependencies.project(":org.metaborg.lang.tiger", Dependency.DEFAULT_CONFIGURATION)
-tigerDependency.artifact {
-  name = tigerDependency.name
-  type = "spoofax-language"
-  extension = "spoofax-language"
-}
-val tigerResources = configurations.create("tigerResources") {
-  isTransitive = false // Don't care about transitive dependencies, just want the Tiger spoofax-language artifact.
-}
-tigerResources.dependencies.add(tigerDependency)
-// First unpack Tiger language resources, because we cannot copy from a subdirectory in a ziptree.
-val unpackResourcesTask = tasks.register<Sync>("unpackResources") {
-  dependsOn(tigerResources)
-  from({ tigerResources.map { project.zipTree(it) } })  /* Closure inside `from` to defer evaluation until task execution time */
-  into("$buildDir/unpacked")
-  include("target/metaborg/editor.esv.af", "target/metaborg/sdf.tbl", "target/metaborg/stratego.jar", "target/metaborg/stratego-javastrat.jar")
-}
-// Copy resources into 'src/main/resources/mb/tiger', so the resources finally end up in the 'mb.tiger' package in the resulting JAR.
-val copyTigerResourcesTask = tasks.register<Sync>("copyTigerResources") {
-  dependsOn(unpackResourcesTask)
-  from("$buildDir/unpacked/target/metaborg")
-  into("src/main/resources/mb/tiger")
-  include("editor.esv.af", "sdf.tbl", "stratego.jar", "stratego-javastrat.jar")
-}
-tasks.getByName(JavaPlugin.PROCESS_RESOURCES_TASK_NAME).dependsOn(copyTigerResourcesTask)
-
-// Copy resources into classes directory, to make them accessible as class resources at runtime.
-val mainSourceSet: SourceSet = sourceSets.main.get()
-val copyResourcesTask = tasks.create<Copy>("copyResources") {
-  from(mainSourceSet.resources.sourceDirectories)
-  into(mainSourceSet.java.outputDir)
-}
-tasks.getByName(JavaPlugin.PROCESS_RESOURCES_TASK_NAME).dependsOn(copyResourcesTask)
-// Same for test resources.
-val testSourceSet: SourceSet = sourceSets.test.get()
-val copyTestResourcesTask = tasks.create<Copy>("copyTestResources") {
-  from(testSourceSet.resources.sourceDirectories)
-  into(testSourceSet.java.outputDir)
-}
-tasks.getByName(JavaPlugin.PROCESS_TEST_RESOURCES_TASK_NAME).dependsOn(copyTestResourcesTask)
