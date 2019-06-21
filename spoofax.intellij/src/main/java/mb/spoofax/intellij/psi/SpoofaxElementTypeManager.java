058c26f49790184591d7f7dc548b32074a2772af
Gabriel Konat <gabrielkonat@gmail.com>
2019-06-21 09:19:26 +0200

    Remove another dependency to javafx.

--- spoofax.intellij/src/main/java/mb/spoofax/intellij/psi/SpoofaxElementTypeManager.java
+++ spoofax.intellij/src/main/java/mb/spoofax/intellij/psi/SpoofaxElementTypeManager.java
@@ -1,16 +1,37 @@
 package mb.spoofax.intellij.psi;
 
 import com.intellij.lang.Language;
-import javafx.util.Pair;
 
 import javax.inject.Inject;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
-
 public final class SpoofaxElementTypeManager {
-    private final List<Pair<String, Supplier<SpoofaxElementType>>> scopes;
+    private final static class ScopeType {
+        final String scope;
+        final Supplier<SpoofaxElementType> typeSupplier;
+
+        private ScopeType(String scope, Supplier<SpoofaxElementType> typeSupplier) {
+            this.scope = scope;
+            this.typeSupplier = typeSupplier;
+        }
+
+        @Override public boolean equals(Object o) {
+            if(this == o) return true;
+            if(o == null || getClass() != o.getClass()) return false;
+            final ScopeType that = (ScopeType) o;
+            if(!scope.equals(that.scope)) return false;
+            return typeSupplier.equals(that.typeSupplier);
+        }
+
+        @Override public int hashCode() {
+            int result = scope.hashCode();
+            result = 31 * result + typeSupplier.hashCode();
+            return result;
+        }
+    }
+    private final List<ScopeType> scopes;
 
     private final SpoofaxElementType rootElementType;
     private final SpoofaxElementType defaultElementType;
@@ -41,13 +62,13 @@ public final class SpoofaxElementTypeManager {
 
     public SpoofaxElementType getElementType(SpoofaxTokenType type) {
         return this.scopes.stream()
-            .filter(p -> type.getScope().startsWith(p.getKey()))
-            .map(p -> p.getValue().get())
+            .filter(p -> type.getScope().startsWith(p.scope))
+            .map(p -> p.typeSupplier.get())
             .findFirst()
             .orElse(defaultElementType);
     }
 
-    private static Pair<String, Supplier<SpoofaxElementType>> scopeType(String prefix, Supplier<SpoofaxElementType> type) {
-        return new Pair<>(prefix, type);
+    private static ScopeType scopeType(String prefix, Supplier<SpoofaxElementType> type) {
+        return new ScopeType(prefix, type);
     }
 }
