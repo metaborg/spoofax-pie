9fcb3c072a2b57c7f556b24201b56b40ed27081f
Gabriel Konat <gabrielkonat@gmail.com>
2020-06-24 16:12:01 +0200

    Implement startup lock rule (in Tiger only)

--- core/spoofax.eclipse/src/main/java/mb/spoofax/eclipse/EclipseLanguageComponent.java
+++ core/spoofax.eclipse/src/main/java/mb/spoofax/eclipse/EclipseLanguageComponent.java
@@ -2,8 +2,11 @@ package mb.spoofax.eclipse;
 
 import mb.spoofax.core.language.LanguageComponent;
 import mb.spoofax.core.language.LanguageScope;
+import mb.spoofax.eclipse.job.LockRule;
 
 @LanguageScope
 public interface EclipseLanguageComponent extends LanguageComponent {
     EclipseIdentifiers getEclipseIdentifiers();
+
+    LockRule startupLockRule();
 }
--- example/tiger/manual/tiger.eclipse/src/main/java/mb/tiger/eclipse/TigerEclipseModule.java
+++ example/tiger/manual/tiger.eclipse/src/main/java/mb/tiger/eclipse/TigerEclipseModule.java
@@ -4,11 +4,13 @@ import dagger.Module;
 import dagger.Provides;
 import mb.spoofax.core.language.LanguageScope;
 import mb.spoofax.eclipse.EclipseIdentifiers;
+import mb.spoofax.eclipse.job.LockRule;
 
 @Module
 public class TigerEclipseModule {
     @Provides @LanguageScope
-    EclipseIdentifiers provideEclipseIdentifiers() {
-        return new TigerEclipseIdentifiers();
-    }
+    EclipseIdentifiers provideEclipseIdentifiers() { return new TigerEclipseIdentifiers(); }
+
+    @Provides @LanguageScope
+    LockRule provideStartupLockRule() { return new LockRule("Startup write lock"); }
 }
