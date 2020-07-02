a75c28b7d435a9d2594c5267ee27c4f75160a55e
Aron Zwaan <aronzwaan@gmail.com>
2020-07-02 14:14:34 +0200

    Provide multilang taskdefs via module

--- core/statix.multilang/src/main/java/mb/statix/multilang/MultiLangModule.java
+++ core/statix.multilang/src/main/java/mb/statix/multilang/MultiLangModule.java
@@ -2,6 +2,20 @@ package mb.statix.multilang;
 
 import dagger.Module;
 import dagger.Provides;
+import dagger.multibindings.ElementsIntoSet;
+import mb.pie.api.TaskDef;
+import mb.statix.multilang.pie.SmlAnalyzeProject;
+import mb.statix.multilang.pie.SmlBuildContextConfiguration;
+import mb.statix.multilang.pie.SmlBuildMessages;
+import mb.statix.multilang.pie.SmlBuildSpec;
+import mb.statix.multilang.pie.SmlInstantiateGlobalScope;
+import mb.statix.multilang.pie.SmlLanguageContext;
+import mb.statix.multilang.pie.SmlPartialSolveFile;
+import mb.statix.multilang.pie.SmlPartialSolveProject;
+import mb.statix.multilang.pie.SmlReadConfigYaml;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Module
 public class MultiLangModule {
@@ -13,5 +27,30 @@ public class MultiLangModule {
 
     @Provides @MultiLangScope public AnalysisContextService provideAnalysisContextService() {
         return analysisContextService;
+    }
+
+    @Provides @MultiLangScope @ElementsIntoSet
+    public Set<TaskDef<?, ?>> provideTaskDefs(
+        SmlAnalyzeProject analyzeProject,
+        SmlBuildContextConfiguration buildContextConfiguration,
+        SmlBuildMessages buildMessages,
+        SmlBuildSpec buildSpec,
+        SmlInstantiateGlobalScope instantiateGlobalScope,
+        SmlLanguageContext languageContext,
+        SmlPartialSolveFile partialSolveFile,
+        SmlPartialSolveProject partialSolveProject,
+        SmlReadConfigYaml readConfigYaml
+    ) {
+        Set<TaskDef<?, ?>> taskDefs = new HashSet<>();
+        taskDefs.add(analyzeProject);
+        taskDefs.add(buildContextConfiguration);
+        taskDefs.add(buildMessages);
+        taskDefs.add(buildSpec);
+        taskDefs.add(instantiateGlobalScope);
+        taskDefs.add(languageContext);
+        taskDefs.add(partialSolveFile);
+        taskDefs.add(partialSolveProject);
+        taskDefs.add(readConfigYaml);
+        return taskDefs;
     }
 }
