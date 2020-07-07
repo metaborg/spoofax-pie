9f27c1157f5a450b3512bc5ce0df3258eba9d976
Gabriel Konat <gabrielkonat@gmail.com>
2020-07-07 16:11:46 +0200

    Delete Tiger generated, as it was causing too much code duplication.

--- example/tiger/generated/tiger.eclipse.externaldeps/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-  id("org.metaborg.gradle.config.java-library")
-  id("org.metaborg.spoofax.compiler.gradle.spoofaxcore.eclipse.externaldeps")
-}
-
-spoofaxEclipseExternaldepsProject {
-  adapterProject.set(project(":tiger.spoofax"))
-}
