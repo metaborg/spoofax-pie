193828d8601bcc717bfa06c5239c305476065922
Aron Zwaan <aronzwaan@gmail.com>
2020-07-13 17:37:46 +0200

    Add utilities for loading metadata components

---
+++ core/statix.multilang/src/main/java/mb/statix/multilang/utils/MetadataUtils.java
@@ -0,0 +1,50 @@
+package mb.statix.multilang.utils;
+
+import mb.pie.api.Function;
+import mb.resource.ResourceKey;
+import mb.resource.classloader.ClassLoaderResource;
+import mb.resource.hierarchical.HierarchicalResource;
+import mb.resource.hierarchical.ResourcePath;
+import mb.resource.hierarchical.match.PathResourceMatcher;
+import mb.resource.hierarchical.match.path.ExtensionsPathMatcher;
+import mb.resource.hierarchical.match.path.NoHiddenPathMatcher;
+import mb.resource.hierarchical.walk.PathResourceWalker;
+import mb.statix.multilang.spec.SpecBuilder;
+import mb.statix.multilang.spec.SpecLoadException;
+import mb.statix.multilang.spec.SpecUtils;
+import org.spoofax.interpreter.terms.ITermFactory;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+public class MetadataUtils {
+
+    private MetadataUtils() {
+    }
+
+    public static SpecBuilder loadSpec(ClassLoaderResource definitionDir, ITermFactory termFactory, String initialModule) {
+        ClassLoaderResource specRoot = definitionDir.appendRelativePath("src-gen/statix");
+        try {
+            return SpecUtils.loadSpec(specRoot, initialModule, termFactory);
+        } catch(IOException e) {
+            throw new RuntimeException(new SpecLoadException("Error loading spec from " + definitionDir, e));
+        }
+    }
+
+    public static Function<ResourcePath, HashSet<ResourceKey>> resourcesSupplierForExtensions(String... extensions) {
+        return (exec, projectDir) -> {
+            HierarchicalResource res = exec.getHierarchicalResource(projectDir);
+            try {
+                return res.walk(
+                    new PathResourceWalker(new NoHiddenPathMatcher()),
+                    new PathResourceMatcher(new ExtensionsPathMatcher(extensions)))
+                    .map(HierarchicalResource::getKey)
+                    .collect(Collectors.toCollection(HashSet::new));
+            } catch(IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        };
+    }
+}
