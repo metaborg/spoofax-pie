11ca9211d94a09d2d37c95425770fddc2879d3b4
Aron Zwaan <aronzwaan@gmail.com>
2020-07-15 20:10:58 +0200

    Remove outdated comment

--- core/statix.multilang/src/main/java/mb/statix/multilang/MultiLangConfig.java
+++ core/statix.multilang/src/main/java/mb/statix/multilang/MultiLangConfig.java
@@ -4,7 +4,6 @@ import org.immutables.value.Value;
 
 import java.util.Map;
 
-// Not immutable since it must have java beans semantics to work with yaml reader (see SmlReadConfigYaml).
 @Value.Immutable
 public interface MultiLangConfig {
     @Value.Parameter Map<LanguageId, ContextId> languageContexts();
