a05a9c145c1dbfd6f992c01557eca8f431581e1b
Aron Zwaan <aronzwaan@gmail.com>
2020-07-15 19:49:28 +0200

    Remove unused method from ContextDataManager

--- core/statix.multilang/src/main/java/mb/statix/multilang/ContextDataManager.java
+++ core/statix.multilang/src/main/java/mb/statix/multilang/ContextDataManager.java
@@ -3,7 +3,6 @@ package mb.statix.multilang;
 import java.util.Set;
 
 public interface ContextDataManager {
-    Set<LanguageId> getAvailableLanguages();
     Set<LanguageId> getContextLanguages(ContextId contextId);
     ContextId getDefaultContextId(LanguageId languageId);
 }
