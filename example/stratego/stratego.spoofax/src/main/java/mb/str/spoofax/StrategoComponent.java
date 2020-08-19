b12db2e8a98959a658673caaecd7b88e6af607e8
Gabriel Konat <gabrielkonat@gmail.com>
2020-08-19 15:45:27 +0200

    Fix missing module.

--- example/stratego/stratego.spoofax/src/main/java/mb/str/spoofax/StrategoComponent.java
+++ example/stratego/stratego.spoofax/src/main/java/mb/str/spoofax/StrategoComponent.java
@@ -3,10 +3,11 @@ package mb.str.spoofax;
 import dagger.Component;
 import mb.spoofax.core.language.LanguageScope;
 import mb.spoofax.core.platform.PlatformComponent;
+import mb.str.spoofax.incr.StrategoIncrModule;
 import mb.str.spoofax.task.StrategoParse;
 
 @LanguageScope
-@Component(modules = {StrategoModule.class}, dependencies = PlatformComponent.class)
+@Component(modules = {StrategoModule.class, StrategoIncrModule.class}, dependencies = PlatformComponent.class)
 public interface StrategoComponent extends GeneratedStrategoComponent {
     StrategoParse getStrategoParse();
 }
