93b9ea5ca80413ea643cfa6dee564f363beb13c6
Gabriel Konat <gabrielkonat@gmail.com>
2020-08-21 14:16:09 +0200

    Test compiler output.

--- example/stratego/stratego.spoofax/src/test/java/mb/str/spoofax/CompileTest.java
+++ example/stratego/stratego.spoofax/src/test/java/mb/str/spoofax/CompileTest.java
@@ -5,9 +5,9 @@ import mb.pie.api.MixedSession;
 import mb.pie.api.None;
 import mb.resource.fs.FSResource;
 import mb.str.spoofax.task.StrategoCompile;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,8 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class CompileTest extends TestBase {
     @Test void testParseTask() throws Exception {
         final FSResource projectDir = rootDirectory.appendRelativePath("project").createDirectory(true);
-        final FSResource mainFile = createTextFile(projectDir, "module a", "a.str");
-        final FSResource outputDir = rootDirectory.appendRelativePath("output").createDirectory(true);
+        final FSResource mainFile = createTextFile(projectDir, "module a rules s = id", "a.str");
+        final FSResource outputDir = rootDirectory.appendRelativePath("output/mb/test").createDirectory(true);
 
         try(final MixedSession session = newSession()) {
             @SuppressWarnings("ConstantConditions") final Result<None, ?> result = session.require(compile.createTask(new StrategoCompile.Args(
@@ -26,11 +26,28 @@ class CompileTest extends TestBase {
                 new ArrayList<>(),
                 null,
                 outputDir.getPath(),
-                "my.package",
+                "mb.test",
                 new ArrayList<>()
             )));
             assertTrue(result.isOk());
             assertTrue(outputDir.exists());
+            assertTrue(outputDir.isDirectory());
+            final FSResource interopRegistererJavaFile = outputDir.appendRelativePath("InteropRegisterer.java");
+            assertTrue(interopRegistererJavaFile.exists());
+            assertTrue(interopRegistererJavaFile.isFile());
+            assertTrue(interopRegistererJavaFile.readString().contains("InteropRegisterer"));
+            final FSResource mainJavaFile = outputDir.appendRelativePath("Main.java");
+            assertTrue(mainJavaFile.exists());
+            assertTrue(mainJavaFile.isFile());
+            assertTrue(mainJavaFile.readString().contains("Main"));
+            final FSResource strategyJavaFile = outputDir.appendRelativePath("s_0_0.java");
+            assertTrue(strategyJavaFile.exists());
+            assertTrue(strategyJavaFile.isFile());
+            assertTrue(strategyJavaFile.readString().contains("s_0_0"));
+            final FSResource testJavaFile = outputDir.appendRelativePath("test.java");
+            assertTrue(testJavaFile.exists());
+            assertTrue(testJavaFile.isFile());
+            assertTrue(testJavaFile.readString().contains("test"));
         }
     }
 }
