be1d41b6b94b79923a46cd1923bc1aeacd31ab32
Aron Zwaan <aronzwaan@gmail.com>
2020-10-01 14:10:45 +0200

    Add multilang spoofaxcore projects

---
+++ example/multilang/spoofaxcore/signature-interface.spoofaxcore/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+  id("org.metaborg.spoofax.gradle.langspec")
+  id("de.set.ecj") // Use ECJ to speed up compilation of Stratego's generated Java files.
+  `maven-publish`
+}
+
+ecj {
+  toolVersion = "3.20.0"
+}
