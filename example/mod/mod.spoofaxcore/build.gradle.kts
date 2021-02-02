5766a46716c230ae039f319142d37146ca496f1a
Gabriel Konat <gabrielkonat@gmail.com>
2021-02-02 15:22:39 +0100

    Use devenv version of SDF3 now that it works.

--- example/mod/mod.spoofaxcore/build.gradle.kts
+++ example/mod/mod.spoofaxcore/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 
 val spoofax2DevenvVersion: String by ext
 dependencies {
+  compileLanguage("org.metaborg.devenv:org.metaborg.meta.lang.template:$spoofax2DevenvVersion")
   compileLanguage("org.metaborg.devenv:org.metaborg.meta.lang.esv:$spoofax2DevenvVersion")
   compileLanguage("org.metaborg.devenv:statix.lang:$spoofax2DevenvVersion")
   sourceLanguage("org.metaborg.devenv:meta.lib.spoofax:$spoofax2DevenvVersion")
