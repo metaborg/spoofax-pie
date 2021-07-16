ca3fcdb64de01bfffff272979bf0d8b1448cadcf
Gabriel Konat <gabrielkonat@gmail.com>
2021-07-16 15:05:09 +0200

    Cap offset/line numbers from tokens to prevent exceptions.
    In particular, the start offset can be -1 when an empty file is parsed
    where the parser does not accept an empty file.

--- core/jsglr.common/src/main/java/mb/jsglr/common/RegionUtil.java
+++ core/jsglr.common/src/main/java/mb/jsglr/common/RegionUtil.java
@@ -5,11 +5,15 @@ import org.spoofax.jsglr.client.imploder.IToken;
 
 public class RegionUtil {
     public static Region fromToken(IToken token) {
-        return Region.fromOffsets(token.getStartOffset(), token.getEndOffset() + 1, token.getLine(), token.getEndLine());
+        return fromTokens(token, token);
     }
 
     public static Region fromTokens(IToken left, IToken right) {
-        return Region.fromOffsets(left.getStartOffset(), right.getEndOffset() + 1, left.getLine(), right.getEndLine());
+        final int startOffset = Math.max(left.getStartOffset(), 0);
+        final int endOffset = Math.max(right.getEndOffset() + 1, startOffset);
+        final int startLine = Math.max(left.getLine(), 0);
+        final int endLine = Math.max(right.getEndLine(), startLine);
+        return Region.fromOffsets(startOffset, endOffset, startLine, endLine);
     }
 
     public static Region fromTokensLayout(IToken left, IToken right, boolean isNullable) {
