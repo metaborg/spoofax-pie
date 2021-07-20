05c62af0c3431aef0b6961d6d79802299be7360b
Gabriel Konat <gabrielkonat@gmail.com>
2021-07-20 15:51:01 +0200

    Update gradle.config to 0.4.6.

--- lwb/build.gradle.kts
+++ lwb/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.4.4"
+  id("org.metaborg.gradle.config.root-project") version "0.4.6"
   id("org.metaborg.gitonium") version "0.1.4"
 
   // Set versions for plugins to use, only applying them in subprojects (apply false here).
