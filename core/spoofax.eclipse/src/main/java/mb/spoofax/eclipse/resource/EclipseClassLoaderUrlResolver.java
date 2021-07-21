a9b80b377d2a3f14776f9fdaef9e375bd4c91e48
Thijs Molendijk <info@thijsmolendijk.nl>
2021-07-21 15:56:36 +0200

    Encode URLs resolved through Eclipse.

--- core/spoofax.eclipse/src/main/java/mb/spoofax/eclipse/resource/EclipseClassLoaderUrlResolver.java
+++ core/spoofax.eclipse/src/main/java/mb/spoofax/eclipse/resource/EclipseClassLoaderUrlResolver.java
@@ -1,10 +1,12 @@
 package mb.spoofax.eclipse.resource;
 
 import mb.resource.classloader.ClassLoaderUrlResolver;
+import mb.resource.util.UriEncode;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.eclipse.core.runtime.FileLocator;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class EclipseClassLoaderUrlResolver implements ClassLoaderUrlResolver {
@@ -12,8 +14,13 @@ public class EclipseClassLoaderUrlResolver implements ClassLoaderUrlResolver {
         try {
             final URL resolvedUrl = FileLocator.resolve(url);
             if(url.equals(resolvedUrl)) return null; // Returns same URL -> URL was not resolved -> return null.
-            return resolvedUrl;
-        } catch(IOException e) {
+
+            // `ClassLoaderUrlResolver` instances are meant to return encoded URLs, but
+            // Eclipse has an outstanding bug from 2006 that makes it return unencoded
+            // values (https://bugs.eclipse.org/bugs/show_bug.cgi?id=145096). Explicitly
+            // encode them here.
+            return UriEncode.encodeToUri(resolvedUrl.toString()).toURL();
+        } catch(IOException | URISyntaxException e) {
             // Resolution failed -> return null.
             return null;
         }
