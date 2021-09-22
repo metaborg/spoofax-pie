af531d09c20cac2ad3ed69853e431c5455e2360d
Gohla <Gohla@users.noreply.github.com>
2021-09-22 13:27:01 +0000

    deploy: 31ed20b14d1a15ddbe19b3d57c6c60ac3f483a06

--- master/macro.py
+++ master/macro.py
@@ -44,6 +44,8 @@ def fill_env_with_release(env, env_version: str, version: str, download_version:
 
 
 release_versions = {
+    "0.11.13": "22-09-2021",
+    "0.11.12": "20-09-2021",
     "0.11.11": "17-09-2021",
     "0.11.10": "15-09-2021",
     "0.11.9": "13-09-2021",
