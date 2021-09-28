5ef98b43eac8bac0d81abb255d883a45798ae508
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-09-29 00:02:47 +0200

    Generate src-gen/statix/statix.merged.aterm

--- lwb/spoofax.lwb.compiler/src/main/java/mb/spoofax/lwb/compiler/statix/CompileStatix.java
+++ lwb/spoofax.lwb.compiler/src/main/java/mb/spoofax/lwb/compiler/statix/CompileStatix.java
@@ -3,6 +3,7 @@ package mb.spoofax.lwb.compiler.statix;
 import mb.cfg.metalang.CompileStatixInput;
 import mb.cfg.task.CfgRootDirectoryToObject;
 import mb.common.message.KeyedMessages;
+import mb.common.message.KeyedMessagesBuilder;
 import mb.common.option.Option;
 import mb.common.result.Result;
 import mb.common.util.ListView;
@@ -11,14 +12,20 @@ import mb.pie.api.Interactivity;
 import mb.pie.api.TaskDef;
 import mb.resource.hierarchical.HierarchicalResource;
 import mb.resource.hierarchical.ResourcePath;
+import mb.spoofax.lwb.compiler.CompileLanguageSpecification;
 import mb.statix.task.StatixCheckMulti;
+import mb.statix.task.StatixCompileAndMergeProject;
 import mb.statix.task.StatixCompileModule;
 import mb.statix.task.StatixCompileProject;
 import mb.statix.task.StatixConfig;
+import org.spoofax.interpreter.terms.IStrategoTerm;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CompileStatix implements TaskDef<ResourcePath, Result<KeyedMessages, StatixCompileException>> {
     private final CfgRootDirectoryToObject cfgRootDirectoryToObject;
@@ -27,17 +34,20 @@ public class CompileStatix implements TaskDef<ResourcePath, Result<KeyedMessages
 
     private final StatixCheckMulti check;
     private final StatixCompileProject compileProject;
+    private final StatixCompileAndMergeProject compileAndMergeProject;
 
     @Inject public CompileStatix(
         CfgRootDirectoryToObject cfgRootDirectoryToObject,
         ConfigureStatix configure,
         StatixCheckMulti check,
-        StatixCompileProject compileProject
+        StatixCompileProject compileProject,
+        StatixCompileAndMergeProject compileAndMergeProject
     ) {
         this.cfgRootDirectoryToObject = cfgRootDirectoryToObject;
         this.configure = configure;
         this.check = check;
         this.compileProject = compileProject;
+        this.compileAndMergeProject = compileAndMergeProject;
     }
 
 
@@ -70,19 +80,54 @@ public class CompileStatix implements TaskDef<ResourcePath, Result<KeyedMessages
             return Result.ofErr(StatixCompileException.checkFail(messages));
         }
 
-        return context.require(compileProject, input.rootDirectory())
+        final HierarchicalResource outputDirectory = context.getHierarchicalResource(input.outputDirectory()).ensureDirectoryExists();
+        final KeyedMessagesBuilder messagesBuilder = new KeyedMessagesBuilder();
+        final Result<KeyedMessages, StatixCompileException> result = context.require(compileProject, input.rootDirectory())
             .mapThrowing(o -> {
-                writeOutput(context, o, input.outputDirectory());
+                writeAllOutputs(context, o, outputDirectory);
                 return messages;
-            }).mapErr(StatixCompileException::compileFail);
+            })
+            .ifOk(messagesBuilder::addMessages)
+            .mapErr(StatixCompileException::compileFail)
+            .and(
+                context.require(compileAndMergeProject, input.rootDirectory())
+                    .mapThrowing(o -> {
+                        final HierarchicalResource mergedOutputFile = outputDirectory.appendAsRelativePath("src-gen/statix/statix.merged.aterm").ensureFileExists();
+                        writeOutput(context, o, mergedOutputFile);
+                        return messages;
+                    })
+                    .ifOk(messagesBuilder::addMessages)
+                    .mapErr(StatixCompileException::compileFail)
+            );
+        if(result.isErr()) return result.ignoreValueIfErr();
+        return Result.ofOk(messagesBuilder.build());
     }
 
-    private void writeOutput(ExecContext context, ListView<StatixCompileModule.Output> compileModuleOutputs, ResourcePath outputPath) throws IOException {
-        final HierarchicalResource outputDirectory = context.getHierarchicalResource(outputPath).ensureDirectoryExists();
+    /**
+     * Writes all compiled Statix modules to files.
+     *
+     * @param context the execution context
+     * @param compileModuleOutputs the compiled Statix modules
+     * @param outputDirectory the path to write the files to
+     * @throws IOException if an I/O exception occurred
+     */
+    private void writeAllOutputs(ExecContext context, ListView<StatixCompileModule.Output> compileModuleOutputs, HierarchicalResource outputDirectory) throws IOException {
         for(StatixCompileModule.Output output : compileModuleOutputs) {
             final HierarchicalResource outputFile = outputDirectory.appendAsRelativePath(output.relativeOutputPath).ensureFileExists();
-            outputFile.writeString(output.spec.toString());
-            context.provide(outputFile);
+            writeOutput(context, output.spec, outputFile);
         }
+    }
+
+    /**
+     * Writes an ATerm to a file.
+     *
+     * @param context the execution context
+     * @param ast the AST to write
+     * @param outputFile the file to write to
+     * @throws IOException if an I/O exception occurred
+     */
+    private void writeOutput(ExecContext context, IStrategoTerm ast, HierarchicalResource outputFile) throws IOException {
+        outputFile.writeString(ast.toString());
+        context.provide(outputFile);
     }
 }
