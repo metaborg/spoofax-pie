87a40b655e49b27ec529e67a121f9123a7a53901
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-10-08 14:27:44 +0200

    Update gradle.config plugin to 0.4.7

--- metalib/build.gradle.kts
+++ metalib/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.4.6"
+  id("org.metaborg.gradle.config.root-project") version "0.4.7"
   id("org.metaborg.gitonium") version "0.1.4"
 
   id("org.metaborg.devenv.spoofax.gradle.langspec") version "0.1.4" apply false
