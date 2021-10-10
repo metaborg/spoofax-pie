d8ed6568352fd01033613f2488b419abbd86207d
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-10-10 12:12:03 +0200

    Publish tiger.spoofax3 and webdsl

--- example/tiger/spoofax3/tiger.spoofax3/build.gradle.kts
+++ example/tiger/spoofax3/tiger.spoofax3/build.gradle.kts
@@ -1,6 +1,7 @@
 import mb.spoofax.compiler.adapter.*
 
 plugins {
+  `maven-publish`
   id("org.metaborg.gradle.config.java-library")
   id("org.metaborg.gradle.config.junit-testing")
   id("org.metaborg.spoofax.lwb.compiler.gradle.language")
@@ -11,4 +12,13 @@ fun compositeBuild(name: String) = "$group:$name:$version"
 dependencies {
   testImplementation(compositeBuild("spoofax.test"))
   testCompileOnly("org.checkerframework:checker-qual-android")
+}
+
+// For ccbench project
+publishing {
+  publications {
+    create<MavenPublication>("mavenJava") {
+       from(components["java"])
+    }
+  }
 }
