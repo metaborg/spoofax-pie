0c0a08354bd175557795373582f87c59177fb1ff
Gabriel Konat <gabrielkonat@gmail.com>
2021-10-13 13:43:35 +0200

    Resolve class loader resources to native Eclipse resources in Eclipse.

--- core/spoofax.eclipse/src/main/java/mb/spoofax/eclipse/resource/EclipseClassLoaderToNativeResolver.java
+++ core/spoofax.eclipse/src/main/java/mb/spoofax/eclipse/resource/EclipseClassLoaderToNativeResolver.java
@@ -6,6 +6,10 @@ import mb.resource.classloader.ClassLoaderToNativeResolver;
 import mb.resource.dagger.ResourceServiceScope;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 
 import javax.inject.Inject;
@@ -16,15 +20,25 @@ import java.net.URL;
 @ResourceServiceScope
 public class EclipseClassLoaderToNativeResolver implements ClassLoaderToNativeResolver {
     private final EclipseResourceRegistry resourceRegistry;
+    private final IWorkspaceRoot workspaceRoot;
 
     @Inject public EclipseClassLoaderToNativeResolver(EclipseResourceRegistry resourceRegistry) {
         this.resourceRegistry = resourceRegistry;
+        this.workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
     }
 
     @Override public @Nullable ReadableResource toNativeResource(URL url) {
         try {
             final URI uri = url.toURI();
-            return null; // TODO: implement
+            final IFile[] files = workspaceRoot.findFilesForLocationURI(uri);
+            if(files.length == 1) {
+                return resourceRegistry.getResource(files[0]);
+            }
+            final IContainer[] containers = workspaceRoot.findContainersForLocationURI(uri);
+            if(containers.length == 1) {
+                return resourceRegistry.getResource(containers[0]);
+            }
+            return null;
         } catch(URISyntaxException e) {
             throw new ResourceRuntimeException("Could not convert URL '" + url + "' to an URI", e);
         }
