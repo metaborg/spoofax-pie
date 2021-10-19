9d753b29b210c4a5238574a388acac2fd53d8893
Gabriel Konat <gabrielkonat@gmail.com>
2021-10-19 10:25:38 +0200

    Add 0.15.0 release to docs.

--- docs/macro.py
+++ docs/macro.py
@@ -44,6 +44,7 @@ def fill_env_with_release(env, env_version: str, version: str, download_version:
 
 
 release_versions = {
+    "0.15.0": "18-10-2021",
     "0.14.2": "13-10-2021",
     "0.14.1": "12-10-2021",
     "0.14.0": "11-10-2021",
