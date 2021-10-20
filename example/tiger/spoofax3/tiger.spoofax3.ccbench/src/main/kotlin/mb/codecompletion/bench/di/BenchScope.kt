5753fabcfc0e6581ee256d1d68a78eebc382a710
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-10-20 15:34:11 +0200

    Remove tiger.ccbench
    The ccbench project is at:
    https://github.com/Virtlink/ccbench

--- example/tiger/spoofax3/tiger.spoofax3.ccbench/src/main/kotlin/mb/codecompletion/bench/di/BenchScope.kt
+++
@@ -1,9 +0,0 @@
-package mb.codecompletion.bench.di
-
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-import javax.inject.Scope
-
-@Scope
-@Retention(RetentionPolicy.RUNTIME)
-annotation class BenchScope
