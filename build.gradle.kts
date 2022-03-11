55526cf0da5f2d942f598ab189fbee945733cb2c
Gabriel Konat <gabrielkonat@gmail.com>
2022-03-11 11:42:07 +0100

    Auto accept build scan TOS and use --scan on buildfarm.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,9 @@ plugins {
   id("org.metaborg.gradle.config.root-project") version "0.4.7"
   id("org.metaborg.gitonium") version "0.1.5"
 }
+
+// Auto-accept build scan TOS
+extensions.findByName("buildScan")?.withGroovyBuilder {
+  setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+  setProperty("termsOfServiceAgree", "yes")
+}
