bf70b9ca63a630f2c5ab4830546513ac3c7a4cc7
Gabriel Konat <gabrielkonat@gmail.com>
2022-04-04 15:38:47 +0200

    Add 0.18.0 release to docs.

--- docs/macro.py
+++ docs/macro.py
@@ -70,6 +70,7 @@ def fill_variables_with_release(variables, env_version: str, version: str, downl
 
 
 release_versions = {
+    "0.18.0": "04-04-2022",
     "0.17.0": "11-03-2022",
     "0.16.17": "13-12-2021",
     "0.16.16": "11-12-2021",
