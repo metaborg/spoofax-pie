5af2e84c446d7c6d39a0d4c3bc4ce2dc4d49133b
Jeff Smits <mail@jeffsmits.net>
2022-06-16 15:14:06 +0200

    Call metaExplode at the end of parse to support mix syntax. Move most shared implementation of StrategoLanguage to the interface.

--- lwb/metalang/stratego/stratego/src/main/java/mb/str/incr/Spoofax3StrategoLanguage.java
+++ lwb/metalang/stratego/stratego/src/main/java/mb/str/incr/Spoofax3StrategoLanguage.java
@@ -18,18 +18,14 @@ import mb.str.StrategoParserSelector;
 import mb.str.StrategoScope;
 import mb.str.task.spoofax.StrategoParseWrapper;
 import mb.stratego.build.strincr.StrategoLanguage;
-import mb.stratego.build.strincr.data.GTEnvironment;
 import mb.stratego.build.termvisitors.DisambiguateAsAnno;
 import mb.stratego.build.util.StrIncrContext;
 import mb.stratego.common.StrategoException;
 import mb.stratego.common.StrategoRuntime;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import org.spoofax.interpreter.terms.IStrategoAppl;
-import org.spoofax.interpreter.terms.IStrategoList;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.ITermFactory;
 import org.spoofax.terms.io.binary.TermReader;
-import org.spoofax.terms.util.TermUtils;
 import org.strategoxt.HybridInterpreter;
 
 import javax.inject.Inject;
@@ -37,7 +33,6 @@ import javax.inject.Provider;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.Collection;
 
 @StrategoScope
@@ -105,73 +100,21 @@ public class Spoofax3StrategoLanguage implements StrategoLanguage {
         }
 
         // Remove ambiguity that occurs in old table from sdf2table when using JSGLR2 parser
-        return new DisambiguateAsAnno(strContext).visit(ast);
+        final IStrategoTerm result = new DisambiguateAsAnno(strContext).visit(ast);
+
+        // Remove mix syntax constructs
+        return metaExplode(result);
     }
 
-    @Override public IStrategoTerm parseRtree(InputStream inputStream) throws Exception {
-        // TODO: reduce code duplication with Spoofax2StrategoLanguage.
-        final IStrategoTerm ast = new TermReader(termFactory).parseFromStream(inputStream);
-        if(!(TermUtils.isAppl(ast) && ((IStrategoAppl)ast).getName().equals("Module") && ast.getSubtermCount() == 2)) {
-            if(TermUtils.isAppl(ast) && ((IStrategoAppl)ast).getName().equals("Specification") && ast.getSubtermCount() == 1) {
-                throw new IOException("Custom library detected with Specification/1 term in RTree file. This is currently not supported");
-            }
-            throw new ExecException("Did not find Module/2 in RTree file. Found: \n" + ast.toString(2));
-        }
-        return ast;
+    @Override public TermReader newTermReader() {
+        return new TermReader(termFactory);
     }
 
-    @Override public IStrategoTerm parseStr2Lib(InputStream inputStream) throws Exception {
-        // TODO: reduce code duplication with Spoofax2StrategoLanguage.
-        final IStrategoTerm ast = new TermReader(termFactory).parseFromStream(inputStream);
-        if(!(TermUtils.isAppl(ast) && ((IStrategoAppl)ast).getName().equals("Str2Lib")
-            && ast.getSubtermCount() == 3)) {
-            throw new ExecException(
-                "Did not find Str2Lib/3 in Str2Lib file. Found: \n" + ast.toString(2));
-        }
-        return ast;
+    @Override public IStrategoTerm makeList(Collection<IStrategoTerm> terms) {
+        return termFactory.makeList(terms);
     }
 
-    @Override
-    public IStrategoTerm insertCasts(String moduleName, GTEnvironment environment, String projectPath) throws ExecException {
-        return callStrategy(environment, projectPath, "stratego2-insert-casts");
-    }
-
-    @Override public IStrategoTerm desugar(IStrategoTerm ast, String projectPath) throws ExecException {
-        return callStrategy(ast, projectPath, "stratego2-compile-top-level-def");
-    }
-
-    @Override public IStrategoTerm toJava(IStrategoList buildInput, String projectPath) throws ExecException {
-        return callStrategy(buildInput, projectPath, "stratego2-strj-sep-comp");
-    }
-
-    @Override public IStrategoAppl toCongruenceAst(IStrategoTerm ast, String projectPath) throws ExecException {
-        return TermUtils.toAppl(callStrategy(ast, projectPath, "stratego2-mk-cong-def"));
-    }
-
-    @Override
-    public Collection<? extends IStrategoAppl> toCongruenceAsts(Collection<IStrategoTerm> asts, String projectPath) throws ExecException {
-        // TODO: reduce code duplication with Spoofax2StrategoLanguage.
-        final IStrategoList result = TermUtils.toList(callStrategy(termFactory.makeList(asts), projectPath, "stratego2-mk-cong-defs"));
-        final ArrayList<IStrategoAppl> congruences = new ArrayList<>(result.size());
-        for(IStrategoTerm t : result) {
-            congruences.add(TermUtils.toAppl(t));
-        }
-        return congruences;
-    }
-
-    @Override public IStrategoTerm auxSignatures(IStrategoTerm ast, String projectPath) throws ExecException {
-        return callStrategy(ast, projectPath, "stratego2-aux-signatures");
-    }
-
-    @Override public IStrategoTerm overlapCheck(IStrategoTerm ast, String projectPath) throws ExecException {
-        return callStrategy(ast, projectPath, "stratego2-dyn-rule-overlap-check");
-    }
-
-    @Override public IStrategoTerm metaExplode(IStrategoTerm ast, String projectPath) throws ExecException {
-        return callStrategy(ast, projectPath, "MetaExplode");
-    }
-
-    private IStrategoTerm callStrategy(IStrategoTerm input, String rootDirectory, String strategyName) throws ExecException {
+    @Override public IStrategoTerm callStrategy(IStrategoTerm input, String rootDirectory, String strategyName) throws ExecException {
         final StrategoRuntime runtime = strategoRuntimeProvider.get();
         final HybridInterpreter hybridInterpreter = runtime.getHybridInterpreter();
         //noinspection deprecation
