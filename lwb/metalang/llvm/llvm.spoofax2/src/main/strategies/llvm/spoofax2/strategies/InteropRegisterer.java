ed26c6129f952fe6a3662fe0928303addf20f79c
Ruben van Baarle <rubnvb@xs4all.nl>
2022-09-27 23:41:51 +0200

    Creation of project for LLVM IR

---
+++ lwb/metalang/llvm/llvm.spoofax2/src/main/strategies/llvm/spoofax2/strategies/InteropRegisterer.java
@@ -0,0 +1,10 @@
+package llvm.spoofax2.strategies;
+
+import org.strategoxt.lang.JavaInteropRegisterer;
+import org.strategoxt.lang.Strategy;
+
+public class InteropRegisterer extends JavaInteropRegisterer {
+    public InteropRegisterer() {
+        super(new Strategy[] { });
+    }
+}
---
+++ lwb/metalang/llvm/llvm.spoofax2/src/main/strategies/llvm/spoofax2/strategies/Main.java
@@ -0,0 +1,9 @@
+package llvm.spoofax2.strategies;
+
+import org.strategoxt.lang.Context;
+
+public class Main {    
+    public static void init(Context context) {
+        
+    }
+}
