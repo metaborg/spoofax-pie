1aea8c7c8d4dd0860496c82087e52d43526f4b47
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2022-10-04 18:14:20 +0200

    Remove deprecated ECJ build dependency

--- example/multilang/spoofaxcore/module-interface.spoofaxcore/build.gradle.kts
+++ example/multilang/spoofaxcore/module-interface.spoofaxcore/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   id("org.metaborg.devenv.spoofax.gradle.langspec")
-  id("de.set.ecj") // Use ECJ to speed up compilation of Stratego's generated Java files.
   `maven-publish`
 }
 
@@ -8,14 +7,6 @@ val spoofax2DevenvVersion: String by ext
 dependencies {
   compileLanguage("org.metaborg.devenv:statix.lang:$spoofax2DevenvVersion")
   sourceLanguage(project(":signature-interface.spoofaxcore"))
-}
-
-ecj {
-  toolVersion = "3.21.0"
-}
-
-tasks.withType<JavaCompile> { // ECJ does not support headerOutputDirectory (-h argument).
-  options.headerOutputDirectory.convention(provider { null })
 }
 
 afterEvaluate {
