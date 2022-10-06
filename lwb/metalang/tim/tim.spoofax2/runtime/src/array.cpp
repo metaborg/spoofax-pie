439620e5989f1c595850f67d83f4d6979f9a7480
Ruben van Baarle <rubnvb@xs4all.nl>
2022-10-06 21:22:55 +0200

    Arrays start at one, fix tailcall issue

--- lwb/metalang/tim/tim.spoofax2/runtime/src/array.cpp
+++ lwb/metalang/tim/tim.spoofax2/runtime/src/array.cpp
@@ -3,26 +3,26 @@
 #include "array.h"
 #include "gc.h"
 
-uint64_t * array_concat(uint64_t *a, uint64_t *b) {
-    auto* fp = get_frame_pointer();
+uint64_t *array_concat(uint64_t *a, uint64_t *b) {
+    auto *fp = get_frame_pointer();
     std::cerr << "ARRAY_CONCAT" << std::endl;
-    uint64_t a_length = a[-1];
-    uint64_t b_length = b[-1];
-    uint64_t new_length = a[-1] + b[-1];
-    auto *result = static_cast<uint64_t *>(gc_alloc_fp((new_length + 1) * sizeof(uint64_t), fp)) + 1;
-    result[-1] = new_length;
-    memcpy(result, a, a_length * sizeof(uint64_t));
-    memcpy(result + a_length, b, b_length * sizeof(uint64_t));
+    uint64_t a_length = a[0];
+    uint64_t b_length = b[0];
+    uint64_t new_length = a[0] + b[0];
+    auto *result = static_cast<uint64_t *>(gc_alloc_fp((new_length + 1) * sizeof(uint64_t), fp));
+    result[0] = new_length;
+    memcpy(result + 1, a + 1, a_length * sizeof(uint64_t));
+    memcpy(result + 1 + a_length, b + 1, b_length * sizeof(uint64_t));
 
     return result;
 }
 
-uint64_t * array_tail(uint64_t *a) {
-    auto* fp = get_frame_pointer();
+uint64_t *array_tail(uint64_t *a) {
+    auto *fp = get_frame_pointer();
     std::cerr << "ARRAY_TAIL" << std::endl;
-    uint64_t new_length = a[-1] - 1;
-    auto *result = static_cast<uint64_t *>(gc_alloc_fp((new_length + 1) * sizeof(uint64_t), fp)) + 1;
-    result[-1] = new_length;
-    memcpy(result, a + 1, new_length * sizeof(uint64_t));
+    uint64_t new_length = a[0] - 1;
+    auto *result = static_cast<uint64_t *>(gc_alloc_fp((new_length + 1) * sizeof(uint64_t), fp));
+    result[0] = new_length;
+    memcpy(result + 1, a + 2, new_length * sizeof(uint64_t));
     return result;
 }
