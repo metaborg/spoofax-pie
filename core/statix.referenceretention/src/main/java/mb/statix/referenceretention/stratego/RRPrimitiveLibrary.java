4b1f2fb4a01d7b3b33e8fb550d2306799f676c49
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-01-23 17:34:02 +0100

    Fix primitive library not being injected

--- core/statix.referenceretention/src/main/java/mb/statix/referenceretention/stratego/RRPrimitiveLibrary.java
+++ core/statix.referenceretention/src/main/java/mb/statix/referenceretention/stratego/RRPrimitiveLibrary.java
@@ -2,8 +2,10 @@ package mb.statix.referenceretention.stratego;
 
 import org.spoofax.interpreter.library.AbstractStrategoOperatorRegistry;
 
+import javax.inject.Inject;
+
 public class RRPrimitiveLibrary extends AbstractStrategoOperatorRegistry {
-    public RRPrimitiveLibrary() {
+    @Inject public RRPrimitiveLibrary() {
         add(new RRFixReferencesStrategy());
         add(new RRCreatePlaceholderStrategy());
         add(new RRLockReferenceStrategy());
