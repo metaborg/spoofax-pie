e021364ab3b68e9c1752aa0e7699f1d0a8bb7e19
Gabriel Konat <gabrielkonat@gmail.com>
2023-06-14 13:48:07 +0200

    Update dependency to fix dependency resolution error.

--- core/spoofax.compiler/build.gradle.kts
+++ core/spoofax.compiler/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
   testImplementation("org.metaborg:pie.runtime")
   testImplementation("org.junit.jupiter:junit-jupiter-params:${metaborg.junitVersion}")
   testImplementation("com.google.jimfs:jimfs:1.1")
-  testImplementation("org.eclipse.jdt:org.eclipse.jdt.core:3.19.0")
+  testImplementation("org.eclipse.jdt:org.eclipse.jdt.core:3.33.0")
   testImplementation("org.gradle:gradle-tooling-api:5.6.4")
   testRuntimeOnly("org.slf4j:slf4j-simple:1.7.10") // SLF4J implementation required for Gradle tooling API.
   testCompileOnly("org.checkerframework:checker-qual-android")
