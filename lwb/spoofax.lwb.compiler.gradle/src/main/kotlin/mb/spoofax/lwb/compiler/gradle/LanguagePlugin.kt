0642b773144b2689e1f61ccce0dd6b2a241797e4
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-22 17:06:03 +0200

    Fixing task dependencies

--- lwb/spoofax.lwb.compiler.gradle/src/main/kotlin/mb/spoofax/lwb/compiler/gradle/LanguagePlugin.kt
+++ lwb/spoofax.lwb.compiler.gradle/src/main/kotlin/mb/spoofax/lwb/compiler/gradle/LanguagePlugin.kt
@@ -371,9 +371,13 @@ class LanguagePluginInstance(
     }
 
     // Make `compileJava` depend on our task, because we generate Java code.
-    project.tasks.getByName(JavaPlugin.COMPILE_JAVA_TASK_NAME).dependsOn(compileTask)
+    project.tasks.named(JavaPlugin.COMPILE_JAVA_TASK_NAME) {
+      dependsOn(compileTask)
+    }
     // Also make `processResources` depend on our task, because we generate resource files.
-    project.tasks.getByName(JavaPlugin.PROCESS_RESOURCES_TASK_NAME).dependsOn(compileTask)
+    project.tasks.named(JavaPlugin.PROCESS_RESOURCES_TASK_NAME) {
+      dependsOn(compileTask)
+    }
   }
 
   private fun ResourcePath.tryAsLocal(name: String, func: (file: File) -> Unit) {
