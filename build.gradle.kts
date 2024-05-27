7c4e6ddb10fdf56ae7717503536a4ef6abc6aab3
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-27 15:01:32 +0200

    Improve delegated project tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,19 +19,21 @@ extensions.findByName("buildScan")?.withGroovyBuilder {
 tasks.register("buildSpoofax3Lwb") {
     dependsOn(gradle.includedBuild("spoofax3.lwb.root").task(":buildAll"))
 }
-tasks.register("runSpoofax3LwbEclipse") {
-    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.feature:runEclipse"))
-}
-tasks.register("buildSpoofax3LwbEclipseInstallation") {
-    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:createEclipseInstallation"))
-}
-tasks.register("buildSpoofax3LwbEclipseInstallationWithJvm") {
-    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:createEclipseInstallationWithJvm"))
-}
-tasks.register("publishSpoofax3Lwb") {
-    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:publish"))
-}
-tasks.register("archiveSpoofax3LwbEclipseInstallations") {
-    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:archiveEclipseInstallations"))
-    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:archiveEclipseInstallationsWithJvm"))
+gradle.includedBuild("spoofax3.lwb.distrib.root").let { lwbDistrib ->
+    tasks.register("runSpoofax3LwbEclipse") {
+        dependsOn(lwbDistrib.task(":spoofax.lwb.eclipse.feature:runEclipse"))
+    }
+    tasks.register("buildSpoofax3LwbEclipseInstallation") {
+        dependsOn(lwbDistrib.task(":spoofax.lwb.eclipse.repository:createEclipseInstallation"))
+    }
+    tasks.register("buildSpoofax3LwbEclipseInstallationWithJvm") {
+        dependsOn(lwbDistrib.task(":spoofax.lwb.eclipse.repository:createEclipseInstallationWithJvm"))
+    }
+    tasks.register("publishSpoofax3Lwb") {
+        dependsOn(lwbDistrib.task(":spoofax.lwb.eclipse.repository:publish"))
+    }
+    tasks.register("archiveSpoofax3LwbEclipseInstallations") {
+        dependsOn(lwbDistrib.task(":spoofax.lwb.eclipse.repository:archiveEclipseInstallations"))
+        dependsOn(lwbDistrib.task(":spoofax.lwb.eclipse.repository:archiveEclipseInstallationsWithJvm"))
+    }
 }
