948661e5103dda19ed975d1c15bd34b627f6d1fb
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-27 14:38:13 +0200

    Add delegated project tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,23 @@ extensions.findByName("buildScan")?.withGroovyBuilder {
         setProperty("termsOfServiceAgree", "yes")
     }
 }
+
+tasks.register("buildSpoofax3Lwb") {
+    dependsOn(gradle.includedBuild("spoofax3.lwb.root").task(":buildAll"))
+}
+tasks.register("runSpoofax3LwbEclipse") {
+    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.feature:runEclipse"))
+}
+tasks.register("buildSpoofax3LwbEclipseInstallation") {
+    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:createEclipseInstallation"))
+}
+tasks.register("buildSpoofax3LwbEclipseInstallationWithJvm") {
+    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:createEclipseInstallationWithJvm"))
+}
+tasks.register("publishSpoofax3Lwb") {
+    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:publish"))
+}
+tasks.register("archiveSpoofax3LwbEclipseInstallations") {
+    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:archiveEclipseInstallations"))
+    dependsOn(gradle.includedBuild("spoofax3.lwb.distrib.root").task(":spoofax.lwb.eclipse.repository:archiveEclipseInstallationsWithJvm"))
+}
