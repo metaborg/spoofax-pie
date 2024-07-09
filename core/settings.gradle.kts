edf2877685e7f94199501f24cab3e2479c4d6d62
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-09 13:52:02 +0200

    Add version catalog

--- core/settings.gradle.kts
+++ core/settings.gradle.kts
@@ -1,11 +1,23 @@
 rootProject.name = "spoofax3.core.root"
 
+// This allows us to use plugins from Metaborg Artifacts
 pluginManagement {
     repositories {
         maven("https://artifacts.metaborg.org/content/groups/public/")
     }
 }
 
+// This allows us to use the catalog in dependencies
+dependencyResolutionManagement {
+    repositories {
+        maven("https://artifacts.metaborg.org/content/groups/public/")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("org.metaborg.spoofax3:catalog:0.3.3")
+        }
+    }
+}
 
 // This downloads an appropriate JVM if not already available
 plugins {
