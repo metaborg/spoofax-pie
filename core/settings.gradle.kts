b43df5066070aa2b9bfe996c91718f65f312a0ea
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-11 17:23:05 +0200

    Use metaborg convention on builds

--- core/settings.gradle.kts
+++ core/settings.gradle.kts
@@ -1,27 +1,13 @@
 rootProject.name = "spoofax3.core.root"
 
-// This allows us to use plugins from Metaborg Artifacts
 pluginManagement {
     repositories {
         maven("https://artifacts.metaborg.org/content/groups/public/")
     }
 }
 
-// This allows us to use the catalog in dependencies
-dependencyResolutionManagement {
-    repositories {
-        maven("https://artifacts.metaborg.org/content/groups/public/")
-    }
-    versionCatalogs {
-        create("libs") {
-            from("org.metaborg.spoofax3:catalog:0.3.3")
-        }
-    }
-}
-
-// This downloads an appropriate JVM if not already available
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.metaborg.convention.settings") version "0.0.13"
 }
 
 include("spoofax.depconstraints")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,15 @@
 rootProject.name = "spoofax3.root"
 
-// This allows us to use plugins from Metaborg Artifacts
 pluginManagement {
     repositories {
         maven("https://artifacts.metaborg.org/content/groups/public/")
     }
 }
 
-// This allows us to use the catalog in dependencies
-dependencyResolutionManagement {
-    repositories {
-        maven("https://artifacts.metaborg.org/content/groups/public/")
-    }
-    versionCatalogs {
-        create("libs") {
-            from("org.metaborg.spoofax3:catalog:0.3.3")
-        }
-    }
+plugins {
+    id("org.metaborg.convention.settings") version "0.0.13"
 }
 
-// This downloads an appropriate JVM if not already available
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
 
 // We split the build up into one main composite build in the 'core' directory, because it builds Gradle plugins,
 // which we want to test. Gradle plugins are not directly available in a multi-project build, therefore a separate
