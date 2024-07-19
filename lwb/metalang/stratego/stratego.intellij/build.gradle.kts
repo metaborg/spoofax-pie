1602871ba9dbdbb21b703efb8dd273b276a173dd
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-19 11:58:42 +0200

    Add missing repositories due to IntelliJ plugin

--- lwb/metalang/stratego/stratego.intellij/build.gradle.kts
+++ lwb/metalang/stratego/stratego.intellij/build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
     id("org.metaborg.spoofax.compiler.gradle.intellij")
 }
 
+// This is a copy of dependencyManagement in the root project's settings.gradle.kts,
+//  which is needed because the IntelliJ plugin (via spoofax.compiler.gradle.intellij) defines its own repository,
+//  overriding those defined in the root dependencyManagement.
+repositories {
+    maven("https://artifacts.metaborg.org/content/groups/public/")
+    mavenCentral()
+}
+
 languageIntellijProject {
     adapterProject.set(project(":stratego"))
 }
