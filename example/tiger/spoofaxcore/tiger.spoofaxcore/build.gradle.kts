cb6e9c3a6fd9d65121ef38dcfe275fbe079df9b5
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-23 16:30:08 +0200

    Use Spoofax 2 artifacts for Tiger

--- example/tiger/spoofaxcore/tiger.spoofaxcore/build.gradle.kts
+++ example/tiger/spoofaxcore/tiger.spoofaxcore/build.gradle.kts
@@ -3,11 +3,27 @@ plugins {
     `maven-publish`
 }
 
+spoofaxLanguageSpecification {
+    addCompileDependenciesFromMetaborgYaml.set(false)
+    addSourceDependenciesFromMetaborgYaml.set(false)
+}
+
+val spoofax2Version: String by ext
 val spoofax2DevenvVersion: String by ext
 dependencies {
+    compileLanguage("org.metaborg:org.metaborg.meta.lang.esv:$spoofax2Version")
+    compileLanguage("org.metaborg:org.metaborg.meta.lang.template:$spoofax2Version")
+    compileLanguage("org.metaborg:org.metaborg.meta.nabl2.lang:$spoofax2Version")
+    compileLanguage("org.metaborg:org.metaborg.meta.nabl2.ext.dynsem:$spoofax2Version")
+    compileLanguage("org.metaborg:dynsem:$spoofax2Version")
     compileLanguage("org.metaborg.devenv:org.metaborg.meta.lang.template:$spoofax2DevenvVersion")
     compileLanguage("org.metaborg.devenv:org.metaborg.meta.lang.esv:$spoofax2DevenvVersion")
     compileLanguage("org.metaborg.devenv:statix.lang:$spoofax2DevenvVersion")
+
+    sourceLanguage("org.metaborg:meta.lib.spoofax:$spoofax2Version")
+    sourceLanguage("org.metaborg:org.metaborg.meta.nabl2.shared:$spoofax2Version")
+    sourceLanguage("org.metaborg:org.metaborg.meta.nabl2.runtime:$spoofax2Version")
     sourceLanguage("org.metaborg.devenv:meta.lib.spoofax:$spoofax2DevenvVersion")
     sourceLanguage("org.metaborg.devenv:statix.runtime:$spoofax2DevenvVersion")
 }
+
